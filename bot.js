4b0eb23741e2014c3c6edeffc67b74c09e41b966
Metunnica <Metunnica@gmail.com>
2018-02-09 16:48:45 +0000

    updated with .env files and a quick reply function

--- bot.js
+++ bot.js
@@ -1,7 +1,29 @@
+import { TwitterBot } from 'node-twitterbot';
+
 // bot.js
 var 
     twit = require('twit');
-    config = require('./config');
+    TwitterBot = require('node-twitterbot').TwitterBot;
+
+var Bot = new TwitterBot ({
+consumer_key: process.env.BOT_CONSUMER_KEY,
+consumer_secret: process.env.BOT_CONSUMER_SECRET,
+access_token: process.env.BOT_ACCESS_TOKEN,
+access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+});
+
+//replys
+
+var phraseArray = [ "Hello Nudie",
+                    "Sup Dog",
+                    "Is the meat in your salad fresh?",
+                    "Dooooooooood Glitch!"
+];
+function chooseRandom(myArray) {
+  return myArray[Math.floor(Math.random() * myArray.length)];
+}
+var phrase = chooseRandom(phraseArray) + ", " + chooseRandom(phraseArray);
+Bot.tweet(phrase);
 
 var Twitter = new twit (config);
 
